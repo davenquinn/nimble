e48b5dce8a40ff8629a9ae716eee218ecc307faa
Daven Quinn <dev@davenquinn.com>
2015-09-11 02:45:25 +0000

    Generalize method for unscaling pixels

--- nimble/__init__.py
+++ nimble/__init__.py
@@ -54,7 +54,13 @@ def align_image(affine, infile, outfile=None):
     offsets = N.array([affine.xoff,affine.yoff])
     px_size = N.array([trans.a,trans.e])
 
-    px_trans = Affine.translation(*offsets/px_size)
+    o = Affine.translation(*offsets)
+    s = Affine.scale(*px_size)
+    px_trans = ~s*o*s
+
+    # Test against simple translation
+    _ = Affine.translation(*offsets/px_size)
+    assert px_trans.almost_equals(_)
 
     if outfile is None:
         outfile = splitext(infile)[0] + ".aligned.vrt"
